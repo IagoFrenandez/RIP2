909a06f268b4b22253ef328d283f0ce369582f7b
IagoFrenandez <71879932+IagoFrenandez@users.noreply.github.com>
2022-05-04 12:06:36 +0200

    IndexMedline

--- src/main/java/es/udc/fi/ri/SearchFiles.java
+++ src/main/java/es/udc/fi/ri/SearchFiles.java
@@ -51,9 +51,13 @@ public class SearchFiles {
 
 	static HashMap<Integer, String> queries = new HashMap<>();
 	static final String ALL_QUERIES = "1-93";
-	static final Path QUERIES_PATH = Paths.get("");
-	static final Path RELEVANCE_PATH = Paths.get("");
+	static final Path QUERIES_PATH = Paths.get("C:\\Users\\iagof\\Desktop\\RI\\med.tar\\MED.QRY");
+	static final Path RELEVANCE_PATH = Paths.get("C:\\Users\\iagof\\Desktop\\RI\\med.tar\\MED.REL");
 	static Path queryFile = QUERIES_PATH;
+	static int queryCount;
+	static int queryMode =0;
+	static String queryRange = "1-3";
+	static String queryNum = "3";
 
 	private SearchFiles() {
 	}
@@ -101,7 +105,7 @@ public class SearchFiles {
 		String indexingmodel = "tfidf";
 		String index = "index";
 		String field = "contents";
-		String queries = null;
+		List<Float> metrics = new ArrayList<>();
 		float lambda = 0.5f;
 		int cut = 1;
 		int top = 1;
@@ -119,7 +123,19 @@ public class SearchFiles {
 				field = args[++i];
 				break;*/
 			case "-queries":
-				queries = args[++i];
+				if(args[i+1] == "all"){
+					queryMode = 0;	//si queryMode = 0, buscamos todas las queries
+					queryRange = ALL_QUERIES;
+					i++;
+				}else if(args[i+1].contains("-")){
+					queryMode = 1; //Buscamos un rango de queries
+					queryRange = args[i+1];
+					i++;
+				}else {
+					queryMode = 2; //Buscamos una query concreta
+					queryNum = args[i+1];
+					i++;
+				}
 				break;
 			case "-top":
 				top = Integer.parseInt(args[++i]);
@@ -131,13 +147,11 @@ public class SearchFiles {
 					System.exit(1);
 				}
 				break;
-				break;
 
 			case "-search":
 				indexingmodel = args[++i];
 				if (indexingmodel.equals("jm"))
 					lambda = Float.valueOf(args[++i]);
-				break;
 				break;
 
 			default:
@@ -149,16 +163,7 @@ public class SearchFiles {
 		DirectoryReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
 		IndexSearcher searcher = new IndexSearcher(reader);
 		Analyzer analyzer = new StandardAnalyzer();
-		KnnVectorDict vectorDict = null;
-		if (knnVectors > 0) {
-			vectorDict = new KnnVectorDict(reader.directory(), IndexMedline.KNN_DICT);
-		}
-		BufferedReader in;
-		if (queries != null) {
-			in = Files.newBufferedReader(Paths.get(queries), StandardCharsets.UTF_8);
-		} else {
-			in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-		}
+
 		QueryParser parser = new QueryParser(field, analyzer);
 		switch (indexingmodel) {
 			case "jm":
@@ -172,28 +177,32 @@ public class SearchFiles {
 				break;
 		}
 
+		if(queryMode == 0){
+			queries.putAll(findQuery(queryRange));
+		}else{
+			queries.putAll(findQuery(queryNum));
+		}
 
-			System.out.println("Searching for: " + query.toString(field));
 		for (Map.Entry<Integer, String> entry : queries.entrySet()) {
 			int num = entry.getKey();
 			String line = entry.getValue();
 			line = line.trim();
 			Query query = parser.parse(line);
 			System.out.println("Searching for: " + query.toString(field));
-			doPagingSearch(searcher, query, num);
+			doPagingSearch(searcher, query, num,cut,top,metrics);
 		}
 		/*if (knnVectors > 0) {
 			query = addSemanticQuery(query, vectorDict, knnVectors);
 		}*/
 		float sum = 0;
-		/*for (Float d : metrics)
+		for (Float d : metrics)
 			sum += d;
-		System.out.println("Mean of the metric for all the queries = " + (float) sum / queryCount);*/
+		System.out.println("Mean of the metric for all the queries = " + (float) sum / queryCount);
 
 
 		reader.close();
 	}
-	}
+
 
 	/**
 	 * This demonstrates a typical paging search scenario, where the search engine
@@ -239,143 +248,64 @@ public class SearchFiles {
 		}
 	}
 
-	public static void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query, int hitsPerPage,
-			boolean raw, boolean interactive) throws IOException {
-
-		// Collect enough docs to show 5 pages
-		TopDocs results = searcher.search(query, 5 * hitsPerPage);
+	public static void doPagingSearch(IndexSearcher searcher, Query query, int num, int cut, int top, List<Float> metrics) throws IOException {
+		TopDocs results = searcher.search(query, cut);
 		ScoreDoc[] hits = results.scoreDocs;
+		List<Integer> relevantDocs = findRelevantDocs(RELEVANCE_PATH, num);
+		Set<Integer> relevantSet = new HashSet<>();
 
+		List<Float> accumPrecision = new ArrayList<>();
+
+		System.out.println("RELEVANT DOCS = " + relevantDocs.toString());
+		System.out.println();
 		int numTotalHits = Math.toIntExact(results.totalHits.value);
 		System.out.println(numTotalHits + " total matching documents");
 
-		int start = 0;
-		int end = Math.min(numTotalHits, hitsPerPage);
-
-		while (true) {
-			if (end > hits.length) {
-				System.out.println("Only results 1 - " + hits.length + " of " + numTotalHits
-						+ " total matching documents collected.");
-				System.out.println("Collect more (y/n) ?");
-				String line = in.readLine();
-				if (line.length() == 0 || line.charAt(0) == 'n') {
-					break;
-				}
-
-				hits = searcher.search(query, numTotalHits).scoreDocs;
-			}
-
-			end = Math.min(hits.length, start + hitsPerPage);
-
-			for (int i = start; i < end; i++) {
-				if (raw) { // output raw format
-					System.out.println("doc=" + hits[i].doc + " score=" + hits[i].score);
-					continue;
-				}
-
-				Document doc = searcher.doc(hits[i].doc);
-				String path = doc.get("path");
-				if (path != null) {
-					System.out.println((i + 1) + ". " + path);
-					String title = doc.get("title");
-					if (title != null) {
-						System.out.println("   Title: " + doc.get("title"));
-					}
-				} else {
-					System.out.println((i + 1) + ". " + "No path for this document");
+		//this loop is used for calculating the metrics
+		int end = Math.min(numTotalHits, cut);
+		int n = 1;
+		for (int i = 0; i < end; i++) {
+			Document doc = searcher.doc(hits[i].doc);
+			int id = Integer.parseInt(doc.get("DocIDNPL"));
+			for (int idaux : relevantDocs) {
+				if (id == idaux) {
+					relevantSet.add(id);
+					float prec = (float) relevantSet.size() / n;
+					accumPrecision.add(prec);
 				}
 			}
-
-			if (!interactive || end == 0) {
-				break;
-			}
-
-			if (numTotalHits >= end) {
-				boolean quit = false;
-				while (true) {
-					System.out.print("Press ");
-					if (start - hitsPerPage >= 0) {
-						System.out.print("(p)revious page, ");
-					}
-					if (start + hitsPerPage < numTotalHits) {
-						System.out.print("(n)ext page, ");
-					}
-					System.out.println("(q)uit or enter number to jump to a page.");
-
-					String line = in.readLine();
-					if (line.length() == 0 || line.charAt(0) == 'q') {
-						quit = true;
-						break;
-					}
-					if (line.charAt(0) == 'p') {
-						start = Math.max(0, start - hitsPerPage);
-						break;
-					} else if (line.charAt(0) == 'n') {
-						if (start + hitsPerPage < numTotalHits) {
-							start += hitsPerPage;
-						}
-						break;
-					} else {
-						int page = Integer.parseInt(line);
-						if ((page - 1) * hitsPerPage < numTotalHits) {
-							start = (page - 1) * hitsPerPage;
-							break;
-						} else {
-							System.out.println("No such page");
-						}
-					}
-				}
-				if (quit)
-					break;
-				end = Math.min(numTotalHits, start + hitsPerPage);
-			}
+			n++;
 		}
+
+		//this is the printing loop, it displays the top TOP hit documents
+		end = Math.min(numTotalHits, top);
+		for (int i = 0; i < end; i++) {
+			Document doc = searcher.doc(hits[i].doc);
+			int id = Integer.parseInt(doc.get("DocIDNPL"));
+			System.out.println((i + 1) + ". Doc ID: " + id + " score = " + hits[i].score);
+		}
+		System.out.println();
+				//P
+				System.out.println("Precision at " + cut + " = " + (float) relevantSet.size() / cut);
+				metrics.add((float) relevantSet.size() / cut);
+				//R
+				System.out.println("Recall at " + cut + " = " + (float) relevantSet.size() / relevantDocs.size());
+				metrics.add((float) relevantSet.size() / relevantDocs.size());
+
+				//MAP
+				if (relevantSet.size() != 0) {
+					float sum = 0;
+					for (Float d : accumPrecision)
+						sum += d;
+					System.out.println("Mean Average Precision at " + cut + " = " + (float) sum / relevantSet.size());
+					metrics.add((float) sum / relevantSet.size());
+				} else
+					System.out.println("Can't compute Mean Average Precision at " + cut + ", no relevant documents found");
+
+
+
+		System.out.println();
+		queryCount++;
 	}
 
-	private static Query addSemanticQuery(Query query, KnnVectorDict vectorDict, int k) throws IOException {
-		StringBuilder semanticQueryText = new StringBuilder();
-		QueryFieldTermExtractor termExtractor = new QueryFieldTermExtractor("contents");
-		query.visit(termExtractor);
-		for (String term : termExtractor.terms) {
-			semanticQueryText.append(term).append(' ');
-		}
-		if (semanticQueryText.length() > 0) {
-			KnnVectorQuery knnQuery = new KnnVectorQuery("contents-vector",
-					new DemoEmbeddings(vectorDict).computeEmbedding(semanticQueryText.toString()), k);
-			BooleanQuery.Builder builder = new BooleanQuery.Builder();
-			builder.add(query, BooleanClause.Occur.SHOULD);
-			builder.add(knnQuery, BooleanClause.Occur.SHOULD);
-			return builder.build();
-		}
-		return query;
-	}
-
-	private static class QueryFieldTermExtractor extends QueryVisitor {
-		private final String field;
-		private final List<String> terms = new ArrayList<>();
-
-		QueryFieldTermExtractor(String field) {
-			this.field = field;
-		}
-
-		@Override
-		public boolean acceptField(String field) {
-			return field.equals(this.field);
-		}
-
-		@Override
-		public void consumeTerms(Query query, Term... terms) {
-			for (Term term : terms) {
-				this.terms.add(term.text());
-			}
-		}
-
-		@Override
-		public QueryVisitor getSubVisitor(BooleanClause.Occur occur, Query parent) {
-			if (occur == BooleanClause.Occur.MUST_NOT) {
-				return QueryVisitor.EMPTY_VISITOR;
-			}
-			return this;
-		}
-	}
 }
